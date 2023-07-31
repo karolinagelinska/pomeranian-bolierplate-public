dcfe77a49ec5e9b6ff036268895f09b73d3aa483
karolinagelinska <karolina@Assus>
2023-07-31 20:03:11 +0200

    DevAndFetch

---
+++ src/App/Exercises/ReactExercises/LocalDevAndFetch/LocalDevAndFetch.jsx
@@ -0,0 +1,5 @@
+import './styles.css';
+
+export const LocalDevAndFetch = () => {
+  return <div>Change me X</div>;
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,9 +1,14 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
-
+import { blockRouterMetaData as blockRouterMetaDataLocalDevAndFetch } from './LocalDevAndFetch/router-data';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 
 import { Block9MetaData } from './Block9/router-data';
 import { MaterialUIMetaData } from './MaterialUIBasicElements/router-data';
 
-export const blockRouterMetaData = [SubRouteExampleMetaData, Block9MetaData, MaterialUIMetaData];
+export const blockRouterMetaData = [
+  blockRouterMetaDataLocalDevAndFetch,
+  SubRouteExampleMetaData,
+  Block9MetaData,
+  MaterialUIMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
